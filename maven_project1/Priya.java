c159c904c724b106ea19a1fb564bc1a09c5a2d7d
Ajay <ajay.thirumalesh@gmail.com>
2024-07-11 14:23:46 +0530

    first commit

--- maven_project1/Priya.java
+++ maven_project1/Priya.java
@@ -3,8 +3,8 @@ package maven_project1;
 public class Priya {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		System.out.println("this is my new world");
 
 	}
 
